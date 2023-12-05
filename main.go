b5ca1e0571d3c7c669b2889d3877d5549d313bd9
kutsikas27 <mardus80@gmail.com>
2023-12-05 13:59:59 +0200

    github push

--- main.go
+++ main.go
@@ -8,7 +8,9 @@ import (
 	"log"
 	"net/http"
 	"path"
+	"sort"
 	"strconv"
+	"strings"
 )
 
 type Body struct {
@@ -27,7 +29,38 @@ type Artist struct {
 	Members      []string `json:"members"`
 }
 
+type ConcertByDate struct {
+	Date     string
+	Location struct {
+		City    string
+		Country string
+	}
+}
+
+func (c ConcertByDate) Less(other ConcertByDate) bool {
+	date1Parts := strings.Split(c.Date, ".")
+	date2Parts := strings.Split(other.Date, ".")
+
+	year1, _ := strconv.Atoi(date1Parts[2])
+	month1, _ := strconv.Atoi(date1Parts[1])
+	day1, _ := strconv.Atoi(date1Parts[0])
+
+	year2, _ := strconv.Atoi(date2Parts[2])
+	month2, _ := strconv.Atoi(date2Parts[1])
+	day2, _ := strconv.Atoi(date2Parts[0])
+
+	if year1 != year2 {
+		return year1 < year2
+	}
+	if month1 != month2 {
+		return month1 < month2
+	}
+	return day1 < day2
+}
+
 func main() {
+	fileServer := http.FileServer(http.Dir("./images"))
+	http.Handle("/images", fileServer)
 	http.HandleFunc("/artist/", artistPageHandler)
 	http.HandleFunc("/", mainPageHandler)
 	fmt.Printf("Starting server at port 8080\n")
@@ -40,8 +73,10 @@ func artistPageHandler(w http.ResponseWriter, r *http.Request) {
 	artist, _ := findArtistByID(artists, id)
 	locationAndDate, _ := getConcertData()
 	locationById, _ := findConcertDataByID(locationAndDate, id)
+
 	if err := renderArtistPage(w, artist, locationById); err != nil {
 		http.Error(w, fmt.Sprintf("Error rendering HTML: %s", err), http.StatusInternalServerError)
+
 	}
 }
 
@@ -53,41 +88,67 @@ func findArtistByID(artists []Artist, id int) (Artist, error) {
 	}
 	return Artist{}, fmt.Errorf("Artist with Id %d not found", id)
 }
-func createStructForDates() {
-	type ConcertByDate struct {
-		Date     []string
-		Location struct {
-			City    string
-			Country string
-		}
-	}
 
-}
-
-func findConcertDataByID(concertData Body, id int) (map[string][]string, error) {
+func findConcertDataByID(concertData Body, id int) ([]ConcertByDate, error) {
 	for _, entry := range concertData.Index {
 		if entry.ID == id {
-			return entry.Concert, nil
+			var concerts []ConcertByDate
+			for city, dates := range entry.Concert {
+				for _, date := range dates {
+					city2, country := extractCities(city)
+					concert := ConcertByDate{
+						Date: normlizeDate(date),
+						Location: struct {
+							City    string
+							Country string
+						}{
+							City:    city2,
+							Country: country,
+						},
+					}
+					concerts = append(concerts, concert)
+				}
+			}
+
+			sort.Slice(concerts, func(i, j int) bool {
+				return concerts[i].Less(concerts[j])
+			})
+
+			return concerts, nil
 		}
 	}
 	return nil, fmt.Errorf("id %d not found", id)
 }
+func normlizeDate(date string) string {
+	splitDate := strings.Split(date, "-")
+	joinDate := strings.Join(splitDate, ".")
 
-//	func extractCities(str string) City {
-//		cities := strings.Split(str, "-")
-//		cities2 := strings.Join(cities, " ")
-//		return strings.Title(cities2)
-//	}
-func renderArtistPage(w http.ResponseWriter, artist Artist, locationAndDate map[string][]string) error {
+	return joinDate
+}
+
+func extractCities(str string) (string, string) {
+	cities := strings.Split(str, "-")
+	cities2 := strings.Join(cities, " ")
+	city := strings.Split(cities2, " ")
+
+	city2 := strings.ToTitle(city[0])
+	country := strings.ToTitle(city[1])
+	return normlizeString(city2), normlizeString(country)
+}
+func normlizeString(str string) string {
+	splitStr := strings.Split(str, "_")
+	return strings.Join(splitStr, " ")
+}
+func renderArtistPage(w http.ResponseWriter, artist Artist, concertData []ConcertByDate) error {
 	data := struct {
-		Artist         Artist
-		DatesLocations map[string][]string
+		Artist   Artist
+		Concerts []ConcertByDate
 	}{
-		Artist:         artist,
-		DatesLocations: locationAndDate,
+		Artist:   artist,
+		Concerts: concertData,
 	}
 
-	htmlTemplate, err := template.ParseFiles("./static/artist.html")
+	htmlTemplate, err := template.ParseFiles("./templates/artist.html")
 	if err != nil {
 		return err
 	}
@@ -125,7 +186,7 @@ func fetchArtists() ([]Artist, error) {
 }
 
 func renderMainPage(w http.ResponseWriter, artists []Artist) error {
-	htmlTemplate, err := template.ParseFiles("./static/index.html")
+	htmlTemplate, err := template.ParseFiles("./templates/index.html")
 	if err != nil {
 		return err
 	}
